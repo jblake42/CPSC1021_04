376389bed359121c41108b439c760ad29feb3bc4
jblake42 <jblakemoore42@gmail.com>
2022-02-14 15:15:07 -0500

    added some classes

---
+++ patientCharges.h
@@ -0,0 +1,17 @@
+#ifndef PATIENTCHARGES_H
+#define PATIENTCHARGES_H
+
+class Patient
+{
+    private:
+        double rate, services, medication;
+        int days;
+        char patientType;
+
+        bool validateInput(int);
+        bool validateInpit(double);
+    public: 
+
+
+};
+#endif
